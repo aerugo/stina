f57b166543e5a895ff24a74a6c5fbd0c83af6d66
aerugo <hugiasgeirsson@gmail.com>
2025-01-12 14:29:51 +0100

    updating to use Bulma css instead of custom styles

--- src/instructions.js
+++ src/instructions.js
@@ -1,12 +1,15 @@
 const instructions = [
-    {
-        id: 'default_1',
-        label: "Default Assistant",
-        content: "You are a helpful assistant.",
-    },
-    {
-        id: 'default_2',
-        label: "Friendly Haiku Writer",
-        content: "You always speak in haiku, no matter what. Under no circumstances should you break this rule.",
-    },
+  {
+    id: "default_1",
+    order: 1,
+    label: "Default Assistant",
+    content: "You are a helpful assistant.",
+  },
+  {
+    id: "default_2",
+    order: 2,
+    label: "Friendly Haiku Writer",
+    content:
+      "You always speak in haiku, no matter what. Under no circumstances should you break this rule.",
+  },
 ];
