8694cb4305198deeeb1c9a9ba779a4919099b48d
aerugo <hugiasgeirsson@gmail.com>
2025-03-02 00:21:33 +0100

    small fix

--- src/api/summaries.js
+++
@@ -1,5 +0,0 @@
-/**
- * Summaries Module (DEPRECATED)
- * This file is kept for backward compatibility.
- * Please use src/summaries/SummariesModule.js and src/summaries/SummariesEvents.js instead.
- */
